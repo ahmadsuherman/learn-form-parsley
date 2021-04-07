c4e4c7027e89f86810e87ac7f0117c9b21a16e31
AhmadSuherman <suhermana274@gmail.com>
2021-04-07 23:30:23 +0700

    Push test code

---
+++ js/js.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){  
+ $('#form-parsley').parsley();
+ 
+ $('#validate_form').on('submit', function(event){
+  event.preventDefault();
+  if($('#validate_form').parsley().isValid())
+  {
+   $.ajax({
+    url:"#",
+    method:"POST",
+    data:$(this).serialize(),
+    beforeSend:function(){
+     $('#submit').attr('disabled','disabled');
+     $('#submit').val('Proses...');
+    },
+    success:function(data)
+    {
+     $('#validate_form')[0].reset();
+     $('#validate_form').parsley().reset();
+     $('#submit').attr('disabled',false);
+     $('#submit').val('Submit');
+     swal("Data berhasil ditambahkan", {
+        button: false,
+        icon: "success",
+        timer: 1000
+     });
+    }
+   });
+  }
+ });
+});  
+
+$(document).ready(function() {
+  $('.btn-refresh').click(function(e) {
+    e.preventDefault();
+    $('.preloader').fadeIn();
+    location.reload();
+  })
+
+})
